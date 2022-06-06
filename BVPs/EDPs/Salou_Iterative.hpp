287cbfc24812f860efa67068c0de415b8622713f
Jorge Morón <jorgemoronvidal@gmail.com>
2022-06-06 18:57:19 +0200

    Changes to be committed: 	modified:   BVPs/EDPs/Salou_Iterative.hpp 	modified:   Solvers/LinIt.m 	modified:   Solvers/LinIt_first.m

--- BVPs/EDPs/Salou_Iterative.hpp
+++ BVPs/EDPs/Salou_Iterative.hpp
@@ -5,8 +5,8 @@
 #include <gsl/gsl_interp2d.h>
 #include <gsl/gsl_spline2d.h>
 #include <gsl/gsl_errno.h>
-#define C2_iteration 6.03
-#define Alpha_iteration 6.7
+#define C2_iteration 4.0
+#define Alpha_iteration 4.4
 //#include "../LUT.hpp"
 inline double Equation_u(Eigen::Vector2d X, double t){
     return  sin(2*M_PI*X(0)+0.5)*cos(M_PI*M_PI*(X(0)+X(1)));
